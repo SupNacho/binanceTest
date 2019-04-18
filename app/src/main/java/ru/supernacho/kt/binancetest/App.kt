956153a0bd469b27ce21af015758dbcc2de058c9
Artem Shumilov <vanbellum@gmail.com>
2019-04-18 10:29:28 +0300

    initialized App

--- app/src/main/java/ru/supernacho/kt/binancetest/App.kt
+++ app/src/main/java/ru/supernacho/kt/binancetest/App.kt
@@ -1,6 +1,28 @@
 package ru.supernacho.kt.binancetest
 
 import android.app.Application
+import com.squareup.leakcanary.LeakCanary
+import io.reactivex.plugins.RxJavaPlugins
+import timber.log.Timber
 
-class App: Application() {
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+        if (LeakCanary.isInAnalyzerProcess(this))
+            return
+        LeakCanary.install(this)
+
+        Timber.plant(Timber.DebugTree())
+
+        RxJavaPlugins.setErrorHandler { e ->
+            Thread.currentThread().uncaughtExceptionHandler.uncaughtException(mainLooper.thread, e)
+        }
+
+    }
+
+    companion object {
+        lateinit var instance: App
+            private set
+    }
 }
